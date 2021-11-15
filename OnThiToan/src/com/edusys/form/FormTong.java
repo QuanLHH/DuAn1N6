470a78ffb0e09a21299058ddc68291fad0e9036c
Ha-Viet-Anh <vietanhsvip@gmail.com>
2021-11-15 17:31:47 +0700

    uptdate 3

--- OnThiToan/src/com/edusys/form/FormTong.java
+++ OnThiToan/src/com/edusys/form/FormTong.java
@@ -9,12 +9,12 @@ package com.edusys.form;
  *
  * @author taola
  */
-public class FormChinh extends javax.swing.JFrame {
+public class FormTong extends javax.swing.JFrame {
 
     /**
      * Creates new form FormChinh
      */
-    public FormChinh() {
+    public FormTong() {
         initComponents();
     }
 
@@ -60,20 +60,21 @@ public class FormChinh extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FormChinh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FormTong.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FormChinh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FormTong.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FormChinh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FormTong.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FormChinh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FormTong.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FormChinh().setVisible(true);
+                new FormTong().setVisible(true);
             }
         });
     }
