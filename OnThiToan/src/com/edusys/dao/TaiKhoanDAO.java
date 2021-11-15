51085dfb1d262a8937f1902d550781c2090dc2cd
hoang <hoang240402@gmail.com>
2021-11-15 23:24:03 +0700

    hoangnh_doimk

--- OnThiToan/src/com/edusys/dao/TaiKhoanDAO.java
+++ OnThiToan/src/com/edusys/dao/TaiKhoanDAO.java
@@ -5,8 +5,12 @@
  */
 package com.edusys.dao;
 
+import Helper.JdbcHelper;
 import PakagesClass.TaiKhoan;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TaiKhoanDAO extends EduSysDAO<TaiKhoan, String> {
 
@@ -29,19 +33,73 @@ public class TaiKhoanDAO extends EduSysDAO<TaiKhoan, String> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
+      public void doiMK(TaiKhoan nv) {
+        String UPDATE = "UPDATE Tai_Khoan SET MatKhau =? WHERE TenTaiKhoan=?";
+        try {
+            JdbcHelper.update(UPDATE, nv.getMatKhau(), nv.getTenTaiKhoan());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+     @Override
     public ArrayList<TaiKhoan> selectALL() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String SELECT = "SELECT * FROM Tai_Khoan";
+        return this.selectBySql(SELECT);
     }
 
+     public ArrayList<TaiKhoan> selectFrom(String sql, Object... args) {
+        ArrayList<TaiKhoan> list = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = JdbcHelper.query(sql, args);
+                while (rs.next()) {
+                    list.add(readFromResultSet(rs));
+                }
+            } finally {
+                rs.getStatement().getConnection().close();      //đóng kết nối từ resultSet
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException();
+        }
+        return list;
+    }
+    
     @Override
     public TaiKhoan selectById(String key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String SelectById = "SELECT*FROM Tai_Khoan WHERE TenTaiKhoan=?";
+        List<TaiKhoan> list = this.selectBySql(SelectById, key);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
     }
 
-    @Override
+  @Override
     protected ArrayList<TaiKhoan> selectBySql(String sql, Object... args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<TaiKhoan> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(sql, args);
+            while (rs.next()) {
+                list.add(readFromResultSet(rs));
+            }
+            rs.getStatement().getConnection().close();
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
     }
 
+     private TaiKhoan readFromResultSet(ResultSet rs) throws SQLException {
+        TaiKhoan model = new TaiKhoan();
+        model.setID_MaND(rs.getInt("ID_MaND"));
+        model.setTenTaiKhoan(rs.getString("TenTaiKhoan"));
+        model.setMatKhau(rs.getString("MatKhau"));
+        model.setMKCap2(rs.getString("MKCap2"));
+        model.setVaiTro(rs.getBoolean("VaiTro"));
+        return model;
+    }
 }
