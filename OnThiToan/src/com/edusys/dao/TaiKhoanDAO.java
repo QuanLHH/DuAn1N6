865c9b04e2ad61cab845f37e5b42024dc93e5d88
Ha-Viet-Anh <vietanhsvip@gmail.com>
2021-11-17 10:22:38 +0700

    finally sprint 2

--- OnThiToan/src/com/edusys/dao/TaiKhoanDAO.java
+++ OnThiToan/src/com/edusys/dao/TaiKhoanDAO.java
@@ -12,16 +12,20 @@ public class TaiKhoanDAO extends EduSysDAO<TaiKhoan, String> {
 
     String INSERT = "INSERT INTO Tai_Khoan (TenTaiKhoan,MatKhau,MKCap2,VaiTro,ID_MaND) VALUES (?,?,?,?,?)";
     String UPDATE = "UPDATE Tai_Khoan SET MatKhau=?,MKCap2=?,VaiTro=?,ID_MaND=? WHERE TenTaiKhoan=?";
+<<<<<<< Updated upstream
+=======
+    String SELECT_BY_ID = "SELECT*FROM Tai_Khoan WHERE TenTaiKhoan=?";
+>>>>>>> Stashed changes
 
     @Override
     public void insert(TaiKhoan tk) {
-        Helper.JdbcHelper.update(INSERT, tk.getTenTaiKhoan(),tk.getMatKhau(),tk.getMKCap2(),tk.getVaiTro(),tk.getID_MaND());
+        Helper.JdbcHelper.update(INSERT, tk.getTenTaiKhoan(), tk.getMatKhau(), tk.getMKCap2(), tk.getVaiTro(), tk.getID_MaND());
 
     }
 
     @Override
     public void update(TaiKhoan tk) {
-        Helper.JdbcHelper.update(UPDATE,tk.getMatKhau(),tk.getMKCap2(),tk.getVaiTro(),tk.getID_MaND(),tk.getTenTaiKhoan());
+        Helper.JdbcHelper.update(UPDATE, tk.getMatKhau(), tk.getMKCap2(), tk.getVaiTro(), tk.getID_MaND(), tk.getTenTaiKhoan());
     }
 
     @Override
@@ -29,11 +33,45 @@ public class TaiKhoanDAO extends EduSysDAO<TaiKhoan, String> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+<<<<<<< Updated upstream
+=======
+    public void doiMK(TaiKhoan nv) {
+        String UPDATE = "UPDATE Tai_Khoan SET MatKhau =? WHERE TenTaiKhoan=?";
+        try {
+            JdbcHelper.update(UPDATE, nv.getMatKhau(), nv.getTenTaiKhoan());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+>>>>>>> Stashed changes
     @Override
     public ArrayList<TaiKhoan> selectALL() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+<<<<<<< Updated upstream
+=======
+    public ArrayList<TaiKhoan> selectFrom(String sql, Object... args) {
+        ArrayList<TaiKhoan> list = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = JdbcHelper.query(sql, args);
+                while (rs.next()) {
+                    list.add(readFromResultSet(rs));
+                }
+            } finally {
+                rs.getStatement().getConnection().close();      //đóng kết nối từ resultSet
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException();
+        }
+        return list;
+    }
+
+>>>>>>> Stashed changes
     @Override
     public TaiKhoan selectById(String key) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -41,7 +79,38 @@ public class TaiKhoanDAO extends EduSysDAO<TaiKhoan, String> {
 
     @Override
     protected ArrayList<TaiKhoan> selectBySql(String sql, Object... args) {
+<<<<<<< Updated upstream
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+=======
+        ArrayList<TaiKhoan> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(sql, args);
+            while (rs.next()) {
+                TaiKhoan tk = new TaiKhoan();
+                tk.setTenTaiKhoan(rs.getString("TenTaiKhoan"));
+                tk.setMatKhau(rs.getString("MatKhau"));
+                tk.setMKCap2(rs.getString("MKCap2"));
+                tk.setVaiTro(rs.getBoolean("VaiTro"));
+                tk.setID_MaND(rs.getInt("ID_MaND"));
+                list.add(tk);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+
+    }
+
+    private TaiKhoan readFromResultSet(ResultSet rs) throws SQLException {
+        TaiKhoan model = new TaiKhoan();
+        model.setID_MaND(rs.getInt("ID_MaND"));
+        model.setTenTaiKhoan(rs.getString("TenTaiKhoan"));
+        model.setMatKhau(rs.getString("MatKhau"));
+        model.setMKCap2(rs.getString("MKCap2"));
+        model.setVaiTro(rs.getBoolean("VaiTro"));
+        return model;
+    }
+>>>>>>> Stashed changes
 }
--- OnThiToan/src/com/edusys/form/JFormSignIn.java
+++ OnThiToan/src/com/edusys/form/JFormSignIn.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.edusys.form;
 
 import PakagesClass.NguoiDung;
@@ -10,20 +5,38 @@ import PakagesClass.TaiKhoan;
 import com.edusys.dao.NguoiDungDAO;
 import com.edusys.dao.TaiKhoanDAO;
 import com.edusys.utils.XDate;
+<<<<<<< Updated upstream:OnThiToan/src/com/edusys/form/FormSignIn.java
 import javax.swing.JOptionPane;
 
+=======
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JOptionPane;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+>>>>>>> Stashed changes:OnThiToan/src/com/edusys/form/JFormSignIn.java
 
 /**
  *
  * @author taola
  */
+<<<<<<< Updated upstream:OnThiToan/src/com/edusys/form/FormSignIn.java
 public class FormSignIn extends javax.swing.JFrame {
+=======
+public class JFormSignIn extends javax.swing.JDialog {
+>>>>>>> Stashed changes:OnThiToan/src/com/edusys/form/JFormSignIn.java
 
     com.edusys.dao.TaiKhoanDAO taiKhoanDAO;
     com.edusys.dao.NguoiDungDAO nguoiDungDAO;
     public static int ID_MaND = 0;
 
+<<<<<<< Updated upstream:OnThiToan/src/com/edusys/form/FormSignIn.java
     public FormSignIn() {
+=======
+    public JFormSignIn(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+>>>>>>> Stashed changes:OnThiToan/src/com/edusys/form/JFormSignIn.java
         initComponents();
         setInit();
         
@@ -33,6 +46,7 @@ public class FormSignIn extends javax.swing.JFrame {
         setLocationRelativeTo(null);
         setResizable(true);
         setTitle("Đăng ký tài khoản");
+<<<<<<< Updated upstream:OnThiToan/src/com/edusys/form/FormSignIn.java
         loandForm();
         taiKhoanDAO = new TaiKhoanDAO();
         nguoiDungDAO = new NguoiDungDAO();
@@ -41,15 +55,28 @@ public class FormSignIn extends javax.swing.JFrame {
     void loandForm() {
         new JFormNDSignIn(this, true).setVisible(true);
     }
+=======
+        taiKhoanDAO = new TaiKhoanDAO();
+        nguoiDungDAO = new NguoiDungDAO();
+    }
+>>>>>>> Stashed changes:OnThiToan/src/com/edusys/form/JFormSignIn.java
 
     public NguoiDung setNguoiDung() {
 
         NguoiDung nd = new NguoiDung();
+<<<<<<< Updated upstream:OnThiToan/src/com/edusys/form/FormSignIn.java
         nd.setHoTen(JFormNDSignIn.getHoTen);
         nd.setGioiTinh(JFormNDSignIn.getGioiTinh);
         nd.setSDT(JFormNDSignIn.getSDT);
         nd.setNgaySinh(XDate.toDate(JFormNDSignIn.getNgaySinh, "dd-MM-yyyy"));
         nd.setEmail(JFormNDSignIn.getEmail);
+=======
+        nd.setHoTen(JFormNguoiDung.getHoTen);
+        nd.setGioiTinh(JFormNguoiDung.getGioiTinh);
+        nd.setSDT(JFormNguoiDung.getSDT);
+        nd.setNgaySinh(XDate.toDate(JFormNguoiDung.getNgaySinh, "dd-MM-yyyy"));
+        nd.setEmail(JFormNguoiDung.getEmail);
+>>>>>>> Stashed changes:OnThiToan/src/com/edusys/form/JFormSignIn.java
         return nd;
     }
 
@@ -68,8 +95,13 @@ public class FormSignIn extends javax.swing.JFrame {
 
     public void insert() {
         try {
+<<<<<<< Updated upstream:OnThiToan/src/com/edusys/form/FormSignIn.java
             
             if(tf_name.getText().equals("")){
+=======
+
+            if (tf_name.getText().equals("")) {
+>>>>>>> Stashed changes:OnThiToan/src/com/edusys/form/JFormSignIn.java
                 JOptionPane.showMessageDialog(rootPane, "Không để trống tài khoản!");
                 return ;
             }else if(tf_pass.getText().equals("")){
@@ -95,7 +127,12 @@ public class FormSignIn extends javax.swing.JFrame {
                 TaiKhoan tk = getForm();
                 taiKhoanDAO.insert(tk);
                 JOptionPane.showMessageDialog(rootPane, "Đăng ký thành công!");
+<<<<<<< Updated upstream:OnThiToan/src/com/edusys/form/FormSignIn.java
                 refresh();
+=======
+                dispose();
+
+>>>>>>> Stashed changes:OnThiToan/src/com/edusys/form/JFormSignIn.java
             }
         } catch (Exception e) {
 
@@ -113,18 +150,24 @@ public class FormSignIn extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        tf_name = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         tf_pass = new javax.swing.JTextField();
         tf_pass2 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         bt_dangky = new javax.swing.JButton();
         bt_Cancel = new javax.swing.JButton();
-        tf_name = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setText("Tên tài khoản:");
+
+        jLabel2.setText("Mật khẩu:");
+
+        jLabel3.setText("Mật khẩu cấp 2:");
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 0, 255));
@@ -144,80 +187,68 @@ public class FormSignIn extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setText("Tên tài khoản:");
-
-        jLabel2.setText("Mật khẩu:");
-
-        jLabel3.setText("Mật khẩu cấp 2:");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
                 .addGap(126, 126, 126)
                 .addComponent(jLabel4)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addGap(118, 118, 118)
                 .addComponent(bt_dangky)
                 .addGap(37, 37, 37)
                 .addComponent(bt_Cancel)
-                .addContainerGap(107, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(86, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
                             .addComponent(jLabel1))
                         .addGap(26, 26, 26))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jLabel3)
                         .addGap(18, 18, 18)))
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(tf_name, javax.swing.GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE)
                     .addComponent(tf_pass)
                     .addComponent(tf_pass2))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(jLabel4)
                 .addGap(42, 42, 42)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(tf_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
                 .addGap(26, 26, 26)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(tf_pass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
                 .addGap(24, 24, 24)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(tf_pass2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(bt_dangky)
                     .addComponent(bt_Cancel))
                 .addGap(41, 41, 41))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bt_dangkyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_dangkyActionPerformed
+        insert();
+
+    }//GEN-LAST:event_bt_dangkyActionPerformed
 
     private void bt_CancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_CancelActionPerformed
         int i = JOptionPane.showConfirmDialog(rootPane, "Bạn có muốn thoát không?", "Thoát", JOptionPane.YES_NO_OPTION);
@@ -227,15 +258,11 @@ public class FormSignIn extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_bt_CancelActionPerformed
 
-    private void bt_dangkyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_dangkyActionPerformed
-        insert();
-        
-    }//GEN-LAST:event_bt_dangkyActionPerformed
-
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+<<<<<<< Updated upstream:OnThiToan/src/com/edusys/form/FormSignIn.java
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -263,9 +290,19 @@ public class FormSignIn extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
+=======
+        /* Create and display the dialog */
+>>>>>>> Stashed changes:OnThiToan/src/com/edusys/form/JFormSignIn.java
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FormSignIn().setVisible(true);
+                JFormSignIn dialog = new JFormSignIn(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
             }
         });
     }
@@ -273,11 +310,11 @@ public class FormSignIn extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bt_Cancel;
     private javax.swing.JButton bt_dangky;
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField tf_name;
     private javax.swing.JTextField tf_pass;
     private javax.swing.JTextField tf_pass2;
