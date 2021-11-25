748cdc0541dbbcce13e59048a37da5ae559b8d5d
hoang <hoang240402@gmail.com>
2021-11-26 00:18:18 +0700

    Update FPoly_DuAn1.sql

--- OnThiToan/src/db/FPoly_DuAn1.sql
+++ OnThiToan/src/db/FPoly_DuAn1.sql
@@ -36,7 +36,11 @@ create table Cau_Hoi(
 	CauHoi nvarchar(250) not null,
 	DoKho int not null,
 	TenBai nvarchar(30),
-	DapAn varchar(2),
+	DapAn1 varchar(250) not null,
+	DapAn2 varchar(250) not null,
+	DapAn3 varchar(250) not null,
+	DapAn4 varchar(250) not null,
+	DapAnDung varchar(2) not null,
 	NgayTao date not null default(getdate())
 )
 go
@@ -99,29 +103,33 @@ VALUES (N'Đạo Hàm','https://www.youtube.com/watch?v=HIGllE3N-iw',
 	(N'Nguyên Hàm','https://www.youtube.com/watch?v=ZgxXaJMg9vQ',
 	'https://vungoi.vn/lop-12/chi-tiet-ly-thuyet-nguyen-ham-5af3eae81261631175a05d3e.html')
 go
-
-INSERT INTO Cau_Hoi(Role_ID,CauHoi,DoKho)
-VALUES (1,'Hinh/dethi/anh1-121.png',1),
-	(1,'Hinh/dethi/anh2-121.png',1),
-	(1,'Hinh/dethi/anh3-121.png',1),
-	(1,'Hinh/dethi/anh2-121.png',2),
-	(1,'Hinh/dethi/anh3-121.png',3)
-
+-- de thi
+INSERT INTO Cau_Hoi(Role_ID,CauHoi,DoKho,TenBai,DapAn1,DapAn2,DapAn3,DapAn4,DapAnDung)
+VALUES (1,N'Câu 1. Tìm giao điểm của đồ thị hàm số (C): y=x^3+x^2-5x+3 và (D):y=x^2+2x-3',1,'',
+    'pi/12','pi/12','5pi/12','5pi/6','C'),
+	(1,N'Câu 2. Tập xác định của hàm số y = 7^(x-2+x−2) là:',2,'',
+	'R','R\{1;−2}','(−2;1).','[2;1].','B'),
+	(1,N'Câu 3. Tập xác định của hàm số y = 3*(x+2)/(x-1) là:',3,'',
+	'R\{1}','(1;+∞).','R','(−∞;1).','D')
 go
-INSERT INTO Cau_Hoi(CauHoi,DoKho,TenBai,DapAn)
-VALUES ('hinh/baitap/bai2.png',1,N'Đạo Hàm','A'),
-	('hinh/baitap/bai1.png',2,N'Đạo Hàm','A'),
-	('hinh/baitap/bai3.png',3,N'Đạo Hàm','A')
-GO
+-- bai tap	
+INSERT INTO Cau_Hoi(Role_ID,CauHoi,DoKho,TenBai,DapAn1,DapAn2,DapAn3,DapAn4,DapAnDung)
+VALUES 
+	(0,N'Câu 1.Tính đạo hàm của các hàm số sau: Tại các điểm được chỉ ra: 1.Cho f(x)= x^3+x-2. Tính f(-2)?',1,N'Đạo Hàm',
+	'f(-2)=13','f(-2)=15','f(-2)=11','f(-2)=12','A'),
+	(0,N'Câu 2.Đạo hàm của hàm số y=(2x^4-3x^2-5x)(x^2-7x) là:',2,N'Đạo Hàm',
+	'(8x^3-6x-5)(x^2-7x)-(2x^4-3x^2-5x)(2x-7)','(8x^3-6x-5)(2x-7)','(8x^3-6x-5)(x^2-7x)+(2x^4-3x^2-5x)(2x-7)','(8x^3-6x-5)+(2x-7)','C'),
+	(0,N'Câu 3.Tại các điểm được chỉ ra: 1.Cho f(x)= x^3+x^2-10. Tính f(4)?',3,N'Đạo Hàm',
+	'f(4)=17','f(4)=20','f(4)=5','f(4)=10','D')
+go
 INSERT INTO Bai_Thi(MaDe,DoKho,ID_CauHoi)
-VALUES('123',1,1),
-	  ('231',2,4),
-	  ('123',1,3),
-	  ('321',3,8)
+VALUES
+	(100,1,1),
+	(200,2,2),
+	(300,3,3)
 SELECT*FROM Nguoi_Dung
 SELECT*FROM Tai_Khoan
 SELECT*FROM Tai_Lieu
 SELECT*FROM Cau_Hoi
-SELECT*FROM Bai_Thi
+SElECT*FROM Bai_Thi
 SELECT TenBai FROM Cau_Hoi GROUP BY TenBai
-
